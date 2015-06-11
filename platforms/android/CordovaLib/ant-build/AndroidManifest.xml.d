e9849ad7357dc4d7f0822ad2599ebb78f6a24800
shreetam-vrisini <shreetam.chatterjee@vrisini.com>
2015-06-11 15:36:07 +0530

    Delete Platform
    Platform Deleted

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Apps\Android_Projects\heel-nederland-deelt\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Apps\Android_Projects\heel-nederland-deelt\platforms\android\CordovaLib\AndroidManifest.xml \
